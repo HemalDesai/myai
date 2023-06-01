be3ae6e4d0ddcf0d362fd74f380d84e68516592b
HemalDesai <hemald332@gmail.com>
2023-06-01 17:58:17 +0530

    first commit

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -28,7 +28,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        const response = await fetch('https://myai-o6rj.onrender.com/api/v1/dalle', {
           method: 'POST',
           // mode: 'no-cors',
           headers: {
@@ -59,7 +59,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://myai-o6rj.onrender.com/api/v1/post', {
           method: 'POST',
           // mode: 'no-cors',
           headers: {
