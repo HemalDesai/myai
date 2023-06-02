e493ef1a05e87a19e18cef5c9de6c91287b23964
HemalDesai <hemald332@gmail.com>
2023-06-02 09:47:23 +0530

    enhancements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-
 import { logo } from './assets';
 import { Home, CreatePost } from './page';
 
 const App = () => (
-  <BrowserRouter>
+ 
+  
+   <BrowserRouter>
     <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
-      <Link to="/">
-        <img src={logo} alt="logo" className="w-28 object-contain" />
+      <Link to="/" className='flex'>
+        <img src="https://i.ibb.co/q00LNFc/ai.png" alt="logo" className="w-10 h-10 " />
+        <h1 className="font-prism text-5xl ml-3">ImaginAI</h1>
+        
       </Link>
 
-      <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
+      <Link to="/create-post" className="font-inter font-medium bg-[#1aadbb] text-white px-4 py-2 rounded-md">Create</Link>
     </header>
     <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
       <Routes>
@@ -19,7 +22,8 @@ const App = () => (
         <Route path="/create-post" element={<CreatePost />} />
       </Routes>
     </main>
-  </BrowserRouter>
+  </BrowserRouter> 
+
 );
 
 export default App;
